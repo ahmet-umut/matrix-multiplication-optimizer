946425ab388629e16c4f14a290f3fb908fd1ce9c
ahmet umut akkaş <95590170+ahmet-umut@users.noreply.github.com>
2025-01-03 09:10:29 +0300

    2,98

--- kernels.c
+++ kernels.c
@@ -5,13 +5,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "defs.h"
+#include <immintrin.h>
 /*
  * Please fill in the following team_t struct
  */
 team_t team = {
 
-        "e2448066",      /* First student ID */
-        "Ahmet Umut",       /* First student name */
+		"e2448066",      /* First student ID */
+		"Ahmet Umut",       /* First student name */
 
 };
 
@@ -29,27 +30,27 @@ team_t team = {
  */
 char naive_normalize_descr[] = "naive_normalize: Naive baseline implementation";
 void naive_normalize(int dim, float *src, float *dst) {
-    float min, max;
-    min = src[0];
-    max = src[0];
+	float min, max;
+	min = src[0];
+	max = src[0];
 
-    for (int i = 0; i < dim; i++) {
-        for (int j = 0; j < dim; j++) {
+	for (int i = 0; i < dim; i++) {
+		for (int j = 0; j < dim; j++) {
 	
-            if (src[RIDX(i, j, dim)] < min) {
-                min = src[RIDX(i, j, dim)];
-            }
-            if (src[RIDX(i, j, dim)] > max) {
-                max = src[RIDX(i, j, dim)];
-            }
-        }
-    }
+			if (src[RIDX(i, j, dim)] < min) {
+				min = src[RIDX(i, j, dim)];
+			}
+			if (src[RIDX(i, j, dim)] > max) {
+				max = src[RIDX(i, j, dim)];
+			}
+		}
+	}
 
-    for (int i = 0; i < dim; i++) {
-        for (int j = 0; j < dim; j++) {
-            dst[RIDX(i, j, dim)] = (src[RIDX(i, j, dim)] - min) / (max - min);
-        }
-    }
+	for (int i = 0; i < dim; i++) {
+		for (int j = 0; j < dim; j++) {
+			dst[RIDX(i, j, dim)] = (src[RIDX(i, j, dim)] - min) / (max - min);
+		}
+	}
 }
 
 /*
@@ -59,31 +60,31 @@ void naive_normalize(int dim, float *src, float *dst) {
 char normalize_descr[] = "Normalize: Current working version";
 void normalize(int dim, float *src, float *dst)
 {
-    float(*srcmatr)[dim] = (float(*)[dim]) src; // cast src to 2D array
-    float(*dstmatr)[dim] = (float(*)[dim]) dst; // cast dst to 2D array
+	float(*srcmatr)[dim] = (float(*)[dim]) src; // cast src to 2D array
+	float(*dstmatr)[dim] = (float(*)[dim]) dst; // cast dst to 2D array
 
-    float min, max;
-    min = src[0];
-    max = src[0];
+	float min, max;
+	min = src[0];
+	max = src[0];
 
-    for (int i = 0; i < dim; i++) {
-        for (int j = 0; j < dim; j++) {
+	for (int i = 0; i < dim; i++) {
+		for (int j = 0; j < dim; j++) {
 	
-            //if (src[RIDX(i, j, dim)] < min)
-            if (srcmatr[i][j] < min)
-                min = src[RIDX(i, j, dim)];
-            //else if (src[RIDX(i, j, dim)] > max)
-            else if (srcmatr[i][j] > max)
-                max = src[RIDX(i, j, dim)];
-        }
-    }
+			//if (src[RIDX(i, j, dim)] < min)
+			if (srcmatr[i][j] < min)
+				min = src[RIDX(i, j, dim)];
+			//else if (src[RIDX(i, j, dim)] > max)
+			else if (srcmatr[i][j] > max)
+				max = src[RIDX(i, j, dim)];
+		}
+	}
 
-    for (int i = 0; i < dim; i++) {
-        for (int j = 0; j < dim; j++) {
-            //dst[RIDX(i, j, dim)] = (src[RIDX(i, j, dim)] - min) / (max - min);
-            dstmatr[i][j] = (srcmatr[i][j] - min) / (max - min);
-        }
-    }
+	for (int i = 0; i < dim; i++) {
+		for (int j = 0; j < dim; j++) {
+			//dst[RIDX(i, j, dim)] = (src[RIDX(i, j, dim)] - min) / (max - min);
+			dstmatr[i][j] = (srcmatr[i][j] - min) / (max - min);
+		}
+	}
 }
 
 /*********************************************************************
@@ -95,9 +96,9 @@ void normalize(int dim, float *src, float *dst)
  *********************************************************************/
 
 void register_normalize_functions() {
-    //add_normalize_function(&naive_normalize, naive_normalize_descr);
-    //add_normalize_function(&normalize, normalize_descr);
-    /* ... Register additional test functions here */
+	//add_normalize_function(&naive_normalize, naive_normalize_descr);
+	//add_normalize_function(&normalize, normalize_descr);
+	/* ... Register additional test functions here */
 }
 
 
@@ -117,15 +118,15 @@ void register_normalize_functions() {
 char naive_kronecker_product_descr[] = "Naive Kronecker Product: Naive baseline implementation";
 void naive_kronecker_product(int dim1, int dim2, float *mat1, float *mat2, float *prod)
 {
-    for (int i = 0; i < dim1; i++) {
-        for (int j = 0; j < dim1; j++) {
-            for (int k = 0; k < dim2; k++) {
-                for (int l = 0; l < dim2; l++) {
-                    prod[RIDX(i, k, dim2) * (dim1 * dim2) + RIDX(j, l, dim2)] = mat1[RIDX(i, j, dim1)] * mat2[RIDX(k, l, dim2)];
-                }
-            }
-        }
-    }
+	for (int i = 0; i < dim1; i++) {
+		for (int j = 0; j < dim1; j++) {
+			for (int k = 0; k < dim2; k++) {
+				for (int l = 0; l < dim2; l++) {
+					prod[RIDX(i, k, dim2) * (dim1 * dim2) + RIDX(j, l, dim2)] = mat1[RIDX(i, j, dim1)] * mat2[RIDX(k, l, dim2)];
+				}
+			}
+		}
+	}
 }
 
 /*
@@ -135,40 +136,48 @@ void naive_kronecker_product(int dim1, int dim2, float *mat1, float *mat2, float
 char kronecker_product_descr[] = "Kronecker Product: Current working version";
 void kronecker_product(int dim1, int dim2, float *mat1, float *mat2, float *prod)
 {
-    float(* matrix1)[dim1] = (float(*)[dim1]) mat1; // cast mat1 to 2D array
-    float(* matrix2)[dim2] = (float(*)[dim2]) mat2; // cast mat2 to 2D array
-    float(* const product)[dim1 * dim2] = (float(*)[dim1 * dim2]) prod; // cast prod to 2D array
+	float(* matrix1)[dim1] = (float(*)[dim1]) mat1; // cast mat1 to 2D array
+	float(* matrix2)[dim2] = (float(*)[dim2]) mat2; // cast mat2 to 2D array
+	float(* const product)[dim1 * dim2] = (float(*)[dim1 * dim2]) prod; // cast prod to 2D array
 
-    for (float(*i)[dim1] = matrix1; i < matrix1 + dim1; i++)
-    {
-        const int value1 = dim2 * (i-matrix1);
-        //for (int j = 0; j < dim1; j++)
+	for (float(*i)[dim1] = matrix1; i < matrix1 + dim1; i++)
+	{
+		const int value1 = dim2 * (i-matrix1);
+		//for (int j = 0; j < dim1; j++)
 		float(* const value9) = *i+dim1;
 		int value8 = 0;
-        for (float(*j) = *i; j < value9; j++)
-        {
-            int value2 = value8++ * dim2;
-            const float value4 = *j;
-            //for (int k = 0; k < dim2; k++)
+		for (float(*j) = *i; j < value9; j++)
+		{
+			int value2 = value8++ * dim2;
+			const float value4 = *j;
+			//for (int k = 0; k < dim2; k++)
 			int value7 = value1;
-            for (float(*k)[dim2] = matrix2; k < matrix2 + dim2; k++)
-            {
-                //const int productrow = k-matrix2 + value1;
-                float(* productrow) = product[value7++];
-                const float (* const value5) = *k+dim2;
-                //for (int l = 0; l < dim2; l++)
+			for (float(*k)[dim2] = matrix2; k < matrix2 + dim2; k++)
+			{
+				//const int productrow = k-matrix2 + value1;
+				float(* productrow) = product[value7++];
+				const float (* const value5) = *k+dim2;
+				//for (int l = 0; l < dim2; l++)
 				int value6 = value2;
-				#pragma GCC unroll 4
-                for (float(*l) = *k; l < value5; l++)
-                {
-                    //prod[RIDX(i, k, dim2) * (dim1 * dim2) + RIDX(j, l, dim2)] = mat1[RIDX(i, j, dim1)] * mat2[RIDX(k, l, dim2)];
-                    //product[i * dim2 + k][j * dim2 + l] = matrix1[i][j] * matrix2[k][l];
-                    //product[(i - matrix1) * dim2 + (k - matrix2)][(j - *i) * dim2 + (l - *k)] = *j * *l;
-                    productrow[value6++] = value4 * *l;
-                }
-            }
-        }
-    }
+
+				/* #pragma GCC unroll 4
+				for (float(*l) = *k; l < value5; l++)
+				{
+					//prod[RIDX(i, k, dim2) * (dim1 * dim2) + RIDX(j, l, dim2)] = mat1[RIDX(i, j, dim1)] * mat2[RIDX(k, l, dim2)];
+					//product[i * dim2 + k][j * dim2 + l] = matrix1[i][j] * matrix2[k][l];
+					//product[(i - matrix1) * dim2 + (k - matrix2)][(j - *i) * dim2 + (l - *k)] = *j * *l;
+					productrow[value6++] = value4 * *l;
+				} */
+				for (float(*l) = *k; l < value5; l+=4, value6+=4)
+				{
+					productrow[value6] = value4 * l[0];
+					productrow[value6+1] = value4 * l[1];
+					productrow[value6+2] = value4 * l[2];
+					productrow[value6+3] = value4 * l[3];
+				}
+			}
+		}
+	}
 }
 
 /******************************************************************************
@@ -180,8 +189,8 @@ void kronecker_product(int dim1, int dim2, float *mat1, float *mat2, float *prod
  ******************************************************************************/
 
 void register_kronecker_product_functions() {
-    //add_kronecker_product_function(&naive_kronecker_product, naive_kronecker_product_descr);
-    add_kronecker_product_function(&kronecker_product, kronecker_product_descr);
-    /* ... Register additional test functions here */
+	//add_kronecker_product_function(&naive_kronecker_product, naive_kronecker_product_descr);
+	add_kronecker_product_function(&kronecker_product, kronecker_product_descr);
+	/* ... Register additional test functions here */
 }
 
