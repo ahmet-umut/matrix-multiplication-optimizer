1aa05e7280473bfb02ff1dadebd97dc2995fc741
ahmet umut akkaş <95590170+ahmet-umut@users.noreply.github.com>
2025-01-03 08:36:34 +0300

    2,05

--- kernels.c
+++ kernels.c
@@ -128,8 +128,6 @@ void naive_kronecker_product(int dim1, int dim2, float *mat1, float *mat2, float
     }
 }
 
-
-
 /*
  * kronecker_product - Your current working version of kronecker_product
  * IMPORTANT: This is the version you will be graded on
@@ -145,20 +143,22 @@ void kronecker_product(int dim1, int dim2, float *mat1, float *mat2, float *prod
     {
         const int value1 = dim2 * (i-matrix1);
         //for (int j = 0; j < dim1; j++)
-        for (float(*j) = *i; j < *i + dim1; j++)
+		float(* const value9) = *i+dim1;
+		int value8 = 0;
+        for (float(*j) = *i; j < value9; j++)
         {
-            const int value2 = (j - *i) * dim2;
+            const int value2 = value8++ * dim2;
             const float value4 = *j;
             //for (int k = 0; k < dim2; k++)
 			int value7 = value1;
-            for (float(*k)[dim2] = matrix2; k < matrix2 + dim2; k++,value7++)
+            for (float(*k)[dim2] = matrix2; k < matrix2 + dim2; k++)
             {
                 //const int value3 = k-matrix2 + value1;
-                float(* value3) = product[value7];
+                float(* value3) = product[value7++];
                 const float (* const value5) = *k+dim2;
                 //for (int l = 0; l < dim2; l++)
 				int value6 = value2;
-                for (float(*l) = *k; l < value5; l++, value6)
+                for (float(*l) = *k; l < value5; l++)
                 {
                     //prod[RIDX(i, k, dim2) * (dim1 * dim2) + RIDX(j, l, dim2)] = mat1[RIDX(i, j, dim1)] * mat2[RIDX(k, l, dim2)];
                     //product[i * dim2 + k][j * dim2 + l] = matrix1[i][j] * matrix2[k][l];
